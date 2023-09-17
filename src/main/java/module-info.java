2f479b85412037dae7ac175a94c586538b16ecb9
Jan-Willem Harmannij <jwharmannij@gmail.com>
2023-09-17 21:54:57 +0200

    Set version to 1.16.2

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,3 +1,8 @@
+/**
+ * This module contains Java language bindings for the <a href="https://www.cairographics.org">cairo</a>
+ * graphics library using the JEP-434 Panama FFI. The bindings are based on <strong>cairo 1.16</strong>
+ * and work with <strong>JDK 20</strong> (with preview features enabled).
+ */
 module org.freedesktop.cairo {
     exports org.freedesktop.cairo;
     exports org.freedesktop.freetype;
